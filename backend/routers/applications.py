85480708904a8d256fe9da2ff0b763df673e0bb9
nikita <nikit15110295@gmail.com>
2026-08-10 00:01:17 +0700

    fix ApplicationResponse

--- backend/routers/applications.py
+++ backend/routers/applications.py
@@ -76,4 +76,4 @@ async def get_by_user(max_user_id: int, session: AsyncSession = Depends(get_sess
 @router.get("/by-phone/{phone}")
 async def by_phone(phone: str, session: AsyncSession = Depends(get_session)):
     application = (await session.execute(select(Application).where(Application.phone == phone))).scalar_one_or_none()
-    return ClientResponse.model_validate(application) if application else None
+    return ApplicationResponse.model_validate(application) if application else None
